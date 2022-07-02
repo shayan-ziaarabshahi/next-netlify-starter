933d937ba5ac7576aae60d5ea3998e496e73ccc5
shayan-ziaarabshahi <72561962+shayan-ziaarabshahi@users.noreply.github.com>
2022-07-02 10:57:44 +0430

    env variables fixed

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    "MONGODB_ATLAS_URI": "mongodb+srv://testUsername:testPassword@cluster0.37hqq.mongodb.net/next-amazon?retryWrites=true&w=majority",
+    "PAYPAL_CLIENT_ID":"AfJ6Ws7w3Wga7qTJgvVlnFEYbMsyo9eUS6qJqgl5eQMWSAXo7M4QZjlnQbY-qSo9wdKSiD99a9SQ2D",
+    "JWT_SECRET":"somePassword"
+  },
 }
 
 module.exports = nextConfig
